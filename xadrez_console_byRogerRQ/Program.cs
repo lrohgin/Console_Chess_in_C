1062ad51b1ca830be10d52dcaa5f01bd6dcd633b
Roger Rodrigues de Queiroz <rogerqueiroz22@hotmail.com>
2021-06-19 19:16:13 -0300

    Projeto no git

--- xadrez_console_byRogerRQ/Program.cs
+++ xadrez_console_byRogerRQ/Program.cs
@@ -7,6 +7,7 @@ namespace xadrez_console_byRogerRQ
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
