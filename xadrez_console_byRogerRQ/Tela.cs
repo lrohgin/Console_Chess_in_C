955763822b2c8d48c7ba7f034b1256e5b37d19fd
Roger Rodrigues de Queiroz <rogerqueiroz22@hotmail.com>
2021-06-23 18:47:28 -0300

    Implementando a verificacao de xeque

--- xadrez_console_byRogerRQ/Tela.cs
+++ xadrez_console_byRogerRQ/Tela.cs
@@ -14,6 +14,10 @@ namespace xadrez_console_byRogerRQ {
             Console.WriteLine();
             Console.WriteLine("Turno: " + partida.turno);
             Console.WriteLine("Aguardando jogada " + partida.jogadorAtual);
+
+            if(partida.xeque) {
+                Console.WriteLine("CHEQUE");
+            }
         }
 
         public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
