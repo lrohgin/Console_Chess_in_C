2e6a7c1379fb73aa87bd047641657bb4ac1b3604
Roger Rodrigues de Queiroz <rogerqueiroz22@hotmail.com>
2021-06-28 22:55:29 -0300

    Jogada especial roque pequeno e roque grande

--- xadrez_console_byRogerRQ/xadrez/Rei.cs
+++ xadrez_console_byRogerRQ/xadrez/Rei.cs
@@ -1,10 +1,12 @@
 ﻿using tabuleiro;
 
-namespace xadrez
-{
-    class Rei : Peca
-    {
-        public Rei(Tabuleiro tab,Cor cor) :base (tab,cor){
+namespace xadrez {
+    class Rei : Peca {
+
+        private PartidaDeXadrez partida;
+        public Rei(Tabuleiro tab,Cor cor,PartidaDeXadrez partida) : base(tab,cor)
+        {
+            this.partida = partida;
         }
 
         public override string ToString()
@@ -12,9 +14,16 @@ namespace xadrez
             return "R";
         }
 
-        private bool podeMover(Posicao pos){
+        private bool podeMover(Posicao pos)
+        {
             Peca p = tab.peca(pos);
             return p == null || p.cor != cor;
+        }
+
+        private bool testeTorreParaRoque(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p != null && p is Torre && p.cor == cor && p.qteMovimentos == 0;
         }
 
         public override bool[,] movimentosPossiveis()
@@ -25,7 +34,7 @@ namespace xadrez
 
             // acima
             pos.definirValores(posicao.linha - 1,posicao.coluna);
-            if ((tab.posicaoValida(pos) && podeMover(pos)) == true) {
+            if ( (tab.posicaoValida(pos) && podeMover(pos)) == true ) {
                 mat[pos.linha,pos.coluna] = true;
             }
             // ne
@@ -62,6 +71,31 @@ namespace xadrez
             pos.definirValores(posicao.linha - 1,posicao.coluna - 1);
             if ( (tab.posicaoValida(pos) && podeMover(pos)) == true ) {
                 mat[pos.linha,pos.coluna] = true;
+            }
+
+            // #jogadaespecial roque
+
+            if ( qteMovimentos == 0 && !partida.xeque ) {
+                //#jogadaespecial roque pequeno
+                Posicao postT1 = new Posicao(posicao.linha,posicao.coluna + 3);
+                if ( testeTorreParaRoque(postT1) ) {
+                    Posicao p1 = new Posicao(posicao.linha,posicao.coluna + 1);
+                    Posicao p2 = new Posicao(posicao.linha,posicao.coluna + 2);
+                    if ( tab.peca(p1) == null && tab.peca(p2) == null ) {
+                        mat[posicao.linha,posicao.coluna + 2] = true;
+                    }
+                }
+
+                //#jogadaespecial roque grande
+                Posicao postT2 = new Posicao(posicao.linha,posicao.coluna - 4);
+                if ( testeTorreParaRoque(postT1) ) {
+                    Posicao p1 = new Posicao(posicao.linha,posicao.coluna - 1);
+                    Posicao p2 = new Posicao(posicao.linha,posicao.coluna - 2);
+                    Posicao p3 = new Posicao(posicao.linha,posicao.coluna - 3);
+                    if ( tab.peca(p1) == null && tab.peca(p2) == null && tab.peca(p3) == null ) {
+                        mat[posicao.linha,posicao.coluna - 2] = true;
+                    }
+                }      
             }
             return mat;
         }
