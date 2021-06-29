680d73405bf8c472769a52338a551761b9752c0f
Roger Rodrigues de Queiroz <rogerqueiroz22@hotmail.com>
2021-06-28 23:02:57 -0300

    Jogada especial Promocao

--- xadrez_console_byRogerRQ/xadrez/PartidaDeXadrez.cs
+++ xadrez_console_byRogerRQ/xadrez/PartidaDeXadrez.cs
@@ -192,7 +192,19 @@ namespace xadrez
             if ( estaEmXeque(jogadorAtual) ) {
                 desfazMovimento(origem,destino,pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque");
-            } 
+            }
+
+            Peca p = tab.peca(destino);
+            //#jogada especial promocao
+            if (p is Peao) {
+                if (p.cor == Cor.Branca && destino.linha == 0 || p.cor == Cor.Preta && destino.linha == 7 ) {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab,p.cor);
+                    tab.colocarPeca(dama,destino);
+                    pecas.Add(dama);
+                }
+            }
 
             if(estaEmXeque(adversaria(jogadorAtual))) {
                 xeque = true;
